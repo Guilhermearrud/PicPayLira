5cdfd2cb314220478bd13fcb3697698c6be3cf7a
Guilherme Lira <guilhermealira@gmail.com>
2024-11-06 21:49:02 -0300

    feat: initial config commit

---
+++ src/main/java/com/picpaylira/picpayLira/PicpayLiraApplication.java
@@ -0,0 +1,13 @@
+package com.picpaylira.picpayLira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicpayLiraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicpayLiraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/picpaylira/picpayLira/PicpayLiraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.picpaylira.picpayLira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpayLiraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
