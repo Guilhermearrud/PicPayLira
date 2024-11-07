f646468224a3f5e0dcc7e27d6f9d15966591eae0
Guilherme Lira <guilhermealira@gmail.com>
2024-11-06 21:49:59 -0300

    feat: create user repository

---
+++ src/main/java/com/picpaylira/picpayLira/repositories/UserRepository.java
@@ -0,0 +1,16 @@
+package com.picpaylira.picpayLira.repositories;
+
+import com.picpaylira.picpayLira.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    // We use option since we are not sure that the user will be found.
+    // Just the declaration is fine because the Jpa repository already knows what to do since we followed a patter in the name of the method.
+    Optional<User> findUserByDocument(String document);
+
+    Optional<User> findUserById(String id);
+
+}
